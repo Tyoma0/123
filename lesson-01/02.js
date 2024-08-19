4a2398a581eece078aab13fbf57c221bcce1836e
Tyoma0 <artyom0070071@mail.ru>
2024-08-19 15:18:31 +0300

    f

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -10,11 +10,11 @@
 
 
 
-let factorial = 1;
-let i = 1;
+let factorial = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
 
 
-while (i <= 10) {
+
+while (factorial <= 10) {
     factorial  *= i;
     i++;
 }
