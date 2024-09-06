5f8466de6157aec76b9e697df1f7450f7a87a0ad
Tyoma0 <artyom0070071@mail.ru>
2024-09-06 20:26:43 +0300

    r

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -20,7 +20,7 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 function findCommonElements(array1,array2) {
     let commonElements =[];
 for ( let item of array1){
-    if (arr2.includes(item)){
+    if (array2.includes(item)){
         commonElements.push(item)
     }
 }
