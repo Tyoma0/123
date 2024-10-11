80219c45e75c8ec4282e9478682ca4e5c8cd498c
Tyoma0 <artyom0070071@mail.ru>
2024-10-11 14:38:56 +0300

    h

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,12 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array,callback) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if(callback(array[i], i)){
+      newArray.push(array[i])
+    }
+    
+  } return newArray
+}
