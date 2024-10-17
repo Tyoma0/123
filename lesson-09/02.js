c0c01074d3d1e49bf5ef373b2a3bd89cbdb76d3c
Tyoma0 <artyom0070071@mail.ru>
2024-10-17 18:02:28 +0300

    h

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -27,21 +27,8 @@ console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]*/
 
 function map(array, callback) {
   const result = []; 
-  for(let i = 0; i < array.length; i++) {
-    
-    const element = array[i]; 
-     const index = i; 
-      result.push(callback(element, index)); 
+  for(let i = 0; i < array.length; i++) { 
+      result.push(callback(array[i], i)); 
   }
   return result; 
 }
-
-
-const numbers = [1, 2, 3, 4, 5];
-
-const doubledNumbers = map(numbers, (element, index) => {
-  return element * 2;
-});
-
-console.log(doubledNumbers);
-
